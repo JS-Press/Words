264e96f755c4b3352af72dc4d61d80c9d12c8fb1
Jarad Solomon <jaradsolomon@gmail.com>
2021-08-29 17:41:39 -0500

    added pumpernickle

--- src/WordCard.js
+++ src/WordCard.js
@@ -23,8 +23,11 @@ function WordCard({ word, handleDelete, feeling }){
 
 
     // console.log(imgUrl)
-
     // const theImage = imgUrl.images.fixed_width.url
+
+    // if(word === "pumpernickle"){
+    //     imgUrl.images.fixed_width.url = "https://media.giphy.com/media/KeseIQWwb0q1G/200.gif"
+    // }
 
     return(
 <>
@@ -33,7 +36,8 @@ function WordCard({ word, handleDelete, feeling }){
 <br></br>
 <p>{word}</p><p onClick={() => handleDelete(word, feeling)}>❌</p>
 <br></br>
-{imgUrl !== 'init' && successful === true && imgUrl.images !== undefined ? <img src= {imgUrl.images.fixed_width.url} alt = {imgUrl.title} /> : <img src="https://media0.giphy.com/media/vnFB72LzHtO4o/giphy.gif?cid=ecf05e47a2z4xt6bkd4pihc7x12iedeitbhucqhnscrr56q4&rid=giphy.gif&ct=g" alt = 'oops nothing here' /> }
+{word !== "pumpernickle" ? imgUrl !== 'init' && successful === true && imgUrl.images !== undefined ? <img src= {imgUrl.images.fixed_width.url} alt = {imgUrl.title} /> : <img src="https://media0.giphy.com/media/vnFB72LzHtO4o/giphy.gif?cid=ecf05e47a2z4xt6bkd4pihc7x12iedeitbhucqhnscrr56q4&rid=giphy.gif&ct=g" alt = 'oops nothing here' /> : null }
+{word === "pumpernickle" ? <img src= "https://media.giphy.com/media/KeseIQWwb0q1G/200.gif" alt = {imgUrl.title} /> : null}
 <br></br>
 <br></br>
 <br></br>
